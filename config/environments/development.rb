ee54b7cffb87753b4abe1d302e667d87ecf3a6cc
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-06-18 11:26:23 +0530

    setup email config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,6 +46,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for mailcatcher gem
+  config.action_mailer.smtp_settings = {
+    address: '127.0.0.1',
+    port: 1025
+  }
 
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,18 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # TODO setup mailer hostname
+  # config.action_mailer.default_url_options = { host: '' }
+
+  # TODO uncomment following settings to setup delivery method
+  # config.action_mailer.delivery_method = :smtp
+
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   domain: 'gmail.com',
+  # }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true

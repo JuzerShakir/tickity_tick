412e72cf5d797c07ee2e4db848b643e32b1fe16d
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-06-18 11:20:46 +0530

    sign in & redirect to homepage after confirming user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "tasks#index"
 
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'users/confirmations' }
 
   post "tasks/:id/toggle", to: "tasks#toggle"
   resources :tasks, except: [:new, :show]

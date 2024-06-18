414ae691b1a0c7bc4efe08ac999552c161f3b580
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-06-18 10:55:39 +0530

    add & setup `Nunito` & `Titillium` fonts

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,22 +1,23 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+	content: [
+		"./public/*.html",
+		"./app/helpers/**/*.rb",
+		"./app/javascript/**/*.js",
+		"./app/views/**/*.{erb,haml,html,slim}",
+		"./app/assets/stylesheets/**/*.css",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Nunito Sans", ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [
+		require("@tailwindcss/forms"),
+		require("@tailwindcss/typography"),
+		require("@tailwindcss/container-queries"),
+	],
+};

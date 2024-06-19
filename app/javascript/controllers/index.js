548284ee70e751df7aeea3c9329dc3482cdbefe8
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-06-19 17:45:19 +0530

    add `character-counter` stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,16 +2,19 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import DialogController from "./dialog_controller"
-application.register("dialog", DialogController)
+import DialogController from "./dialog_controller";
+application.register("dialog", DialogController);
 
-import FlashController from "./flash_controller"
-application.register("flash", FlashController)
+import FlashController from "./flash_controller";
+application.register("flash", FlashController);
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HelloController from "./hello_controller";
+application.register("hello", HelloController);
 
-import TasksController from "./tasks_controller"
-application.register("tasks", TasksController)
+import TasksController from "./tasks_controller";
+application.register("tasks", TasksController);
+
+import CharacterCounter from "@stimulus-components/character-counter";
+application.register("character-counter", CharacterCounter);

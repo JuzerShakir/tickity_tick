f6279eddd2fba6e2536ae0fb43ee8d2aad86c88c
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-06-20 10:12:10 +0530

    change after sign out path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-    tasks_path
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    home_path
   end
 end

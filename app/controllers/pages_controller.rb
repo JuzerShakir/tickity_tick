e125f4791b5754af6f19435de19458ae7a033ea1
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-06-20 10:13:38 +0530

    restrict access to `pages#home` for signed_in users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
-  def home
+  before_action :redirect_if_logged_in
+
+  def home;end
+
+  private
+
+  def redirect_if_logged_in
+    if user_signed_in?
+      redirect_to root_path, alert: "You need to sign out before continuing."
+    end
   end
 end

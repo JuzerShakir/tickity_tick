73261be49620a46e8b38a93fd0d112c931423323
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-08-21 20:34:10 +0530

    whitelist `name` attribute when updating user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
   private
+
   def after_sign_in_path_for(resource)
     tasks_path
   end

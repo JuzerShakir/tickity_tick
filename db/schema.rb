d36a7b7fd49c963df0a4d5b3bdadab1aff2c3911
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-08-21 23:43:41 +0530

    set `not_null` constraint to `true` for name attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_21_144023) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_21_181048) do
   create_table "tasks", force: :cascade do |t|
     t.string "description", limit: 100, null: false
     t.boolean "completed", default: false
@@ -32,8 +32,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_21_144023) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name", default: "", null: false
-    t.string "last_name", default: "", null: false
+    t.string "first_name", default: ""
+    t.string "last_name", default: ""
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
